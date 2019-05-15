8e499e6f974c0346324f34e44c6683b35de47732
Luísa Barros <qlullyq@gmail.com>
2019-05-15 18:25:41 -0300

    Validate props

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Repository } from './styles';
 
@@ -30,5 +31,9 @@ const CompareList = ({ repositories }) => (
     ))}
   </Container>
 );
+
+CompareList.propTypes = {
+  repositories: PropTypes.objectOf(PropTypes.shape).isRequired,
+};
 
 export default CompareList;
